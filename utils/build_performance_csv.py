948399889307ab26c1dc0cc0dee76ea2dd6b73dc
Daniel White <onager@deerpie.com>
2017-09-14 17:10:30 +0200

    WIP on performance dashboard

--- utils/build_performance_csv.py
+++ utils/build_performance_csv.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from __future__ import print_function
 
 import argparse
 import datetime
 import csv
 import os
-import re
 from plaso.cli import pinfo_tool
 from plaso.cli import tools
 import plaso.lib.errors
@@ -16,10 +14,9 @@ from google.cloud import storage
 
 
 class PlasoCIFetcher(object):
-  RESULTS_ROOT = 'jenkins/build_results'
+  RESULTS_ROOT = 'build_results'
 
-  def __init__(self, bucket_name='', project_name='',
-      storage_file_temporary_directory=''):
+  def __init__(self, bucket_name='', project_name='', storage_file_temporary_directory=''):
     super(PlasoCIFetcher, self).__init__()
     self._storage_file_temporary_directory = storage_file_temporary_directory
     self._bucket_name = bucket_name
@@ -47,8 +44,8 @@ class PlasoCIFetcher(object):
   def DownloadStorageFiles(self, test_name):
     """Downloads all the storage files for a given test."""
     # downloaded_files = list()
-    storage_file_dir = os.path.join(self._storage_file_temporary_directory,
-        test_name)
+    storage_file_dir = os.path.join(
+        self._storage_file_temporary_directory, test_name)
     try:
       os.makedirs(storage_file_dir)
     except OSError:
@@ -77,26 +74,15 @@ class PlasoCIFetcher(object):
     translated_name = translated_name.replace(storage_file_dir, '')
     storage_client = storage.Client(project=self._project_name)
     bucket = storage_client.get_bucket(self._bucket_name)
+    # blob_name = "{0:s}/{1:s}".format(self.RESULTS_ROOT, translated_name)
     print('Uploading file {0:s}'.format(filename))
     blob = bucket.blob(translated_name)
     blob.upload_from_filename(filename)
 
 
-def ProcessTest(test_name, project_name='', bucket_name='',
-    storage_file_temporary_directory=''):
-  """
-
-  Args:
-    test_name:
-    project_name:
-    storage_file_temporary_directory:
-
-  Returns:
-
-  """
-  fetcher = PlasoCIFetcher(project_name=project_name,
-      storage_file_temporary_directory=storage_file_temporary_directory,
-      bucket_name=bucket_name)
+def ProcessTest(test_name, project_name, storage_file_temporary_directory):
+  fetcher = PlasoCIFetcher(
+      project_name=project_name, storage_file_temporary_directory=storage_file_temporary_directory)
   for storage_file in fetcher.DownloadStorageFiles(test_name):
     filename, _, _ = storage_file.partition('.')
     output_path = '{0:s}.{1:s}'.format(filename, 'json')
@@ -129,35 +115,26 @@ def BuildCSV(test_name, storage_file_temporary_directory, metric_file_name):
 
   """
   path = storage_file_temporary_directory
-  fieldnames = ['build_number', 'start_date', 'elapsed_time',
-    'number_of_parsers', 'total_events']
+  fieldnames = [
+    'start_date', 'elapsed_time', 'number_of_parsers', 'total_events']
   metrics_rows = []
-  storage_file_dir = os.path.join(storage_file_temporary_directory, test_name)
+  storage_file_dir = os.path.join(storage_file_temporary_directory,
+      test_name)
   for filename in os.listdir(storage_file_dir):
-    file_path = '{0:s}/{1:s}/{2:s}'.format(path, test_name, filename)
+    file_path = '{0:s}{1:s}/{2:s}'.format(path, test_name, filename)
     if 'json' not in filename:
       continue
     if 'gold' in filename:
       continue
-    build_number_match = re.search('!(\d+)!', filename)
-    build_number = 0
-    if build_number_match:
-      build_number = build_number_match.group(1)
-
     with open(file_path, 'r') as metric_file:
-      try:
-        results = json.load(metric_file)
-      except ValueError:
-        print('Couldn\'t load {0:s}'.format(filename))
-        continue
+      results = json.load(metric_file)
       session = results.items()[0][1]
       metrics_row = {}
       start_date = datetime.datetime.utcfromtimestamp(
           session['start_time'] / 1000000)
       elapsed_time = (session['completion_time'] - session[
         'start_time']) / 100000
-      enabled_parser_names = session.get('enabled_parser_names', [])
-      number_of_parsers = len(enabled_parser_names)
+      number_of_parsers = len(session['enabled_parser_names'])
 
       # Add counts from parsers
       for parser_name, event_count in session['parsers_counter'].items():
@@ -172,7 +149,6 @@ def BuildCSV(test_name, storage_file_temporary_directory, metric_file_name):
       events_counter = session['parsers_counter'].values()
       events_counter.remove('collections.Counter')
       total_events = sum(events_counter)
-      metrics_row['build_number'] = build_number
       metrics_row['start_date'] = start_date
       metrics_row['elapsed_time'] = elapsed_time
       metrics_row['number_of_parsers'] = number_of_parsers
@@ -181,7 +157,6 @@ def BuildCSV(test_name, storage_file_temporary_directory, metric_file_name):
 
   metric_file_name = os.path.join('/tmp', metric_file_name)
   with open(metric_file_name, b'w') as csvfile:
-    # TODO: ad build number and git hash
     csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames, restval=0)
     csvwriter.writeheader()
     rows = sorted(metrics_rows)
@@ -192,13 +167,16 @@ def BuildCSV(test_name, storage_file_temporary_directory, metric_file_name):
 if __name__ == '__main__':
   argument_parser = argparse.ArgumentParser()
 
-  argument_parser.add_argument('temporary_directory', type=str,
+  argument_parser.add_argument(
+      'temporary_directory', type=str,
       help='Path to a temporary directory to cache storage files')
 
-  argument_parser.add_argument('project_name', type=str,
+  argument_parser.add_argument(
+      'project_name', type=str,
       help='Project where the tests were run')
 
-  argument_parser.add_argument('bucket_name', type=str,
+  argument_parser.add_argument(
+      'bucket_name', type=str,
       help='Bucket where test results are stored')
 
   options = argument_parser.parse_args()
@@ -207,15 +185,8 @@ if __name__ == '__main__':
     'plaso_registrar_end_to_end', 'plaso_studentpc1_end_to_end',
     'plaso_dean_end_to_end', 'plaso_acserver_end_to_end',
     'plaso_end_to_end_windows_studentpc1']
-  # test_names = ['plaso_registrar_end_to_end']
-  # test_names = ['plaso-e2e-registrar-sqlite', 'plaso-linux-e2e-acserver',
-  #              'plaso-linux-e2e-dean', 'plaso-linux-e2e-registrar',
-  #              'plaso-linux-e2e-studentpc1-experimental',
-  #              'plaso-linux-e2e-studentpc1']
   for test in test_names:
-    ProcessTest(test, project_name=options.project_name,
-        bucket_name=options.bucket_name,
-        storage_file_temporary_directory=options.temporary_directory)
+    ProcessTest(test, project_name=options.project_name, )
     output_name = '{0:s}_metrics.csv'.format(test)
     BuildCSV(test, storage_file_temporary_directory=options.temporary_directory,
-        metric_file_name=output_name)
+    metric_file_name=output_name)
